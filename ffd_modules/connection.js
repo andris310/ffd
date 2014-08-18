64e75ab7e56ccddb72b1e624c74007040386db86
andris310 <andris310@gmail.com>
2014-08-17 19:08:18 -0700

    add mongolab support

--- ffd_modules/connection.js
+++ ffd_modules/connection.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var dbConfig = require('../config/db');
 
-mongoose.connect(dbConfig.url);
+mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGO_URL);
 var db;
 exports.db = function() {
   if (db === null) {
