5b4f02b001511da0f4d4f898b55940b4005751e0
andris310 <andris310@gmail.com>
2014-08-31 14:42:12 -0700

    fix logo size

--- routes/staticRt.js
+++ routes/staticRt.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('home', {
     title: 'Famous Furniture Design',
-    metaDescription: 'Collection of most notable and famous furniture design and their designers.',
+    metaDescription: 'A quick guide to some of the most famous furniture designs and designers that created these peaces of art. Most of the furniture featured on this site was created arround mid 20th century. They stand through time and their design has become iconic.',
     alias: 'home',
     user : req.user
   });
